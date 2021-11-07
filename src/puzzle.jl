98d944628686a5ffacd0b429125b32d93c89a861
Joshua-Robison <joshua-robison@outlook.com>
2021-11-06 22:42:20 -0400

    main

--- src/puzzle.jl
+++ src/puzzle.jl
@@ -3,27 +3,27 @@ export Puzzle
 
 # Puzzle Structure
 struct Puzzle
-    grid::Matrix{Int} # n x n matrix
-    function Puzzle(grid::Matrix{Int})
+    board::Matrix{Int} # n x n matrix
+    function Puzzle(board::Matrix{Int})
         # verify input is a valid Sudoku puzzle (9 x 9)
-        @assert size(grid) == (9, 9) "Invalid puzzle dimensions!"
-        return new(grid);
+        @assert size(board) == (9, 9) "Invalid puzzle dimensions!"
+        return new(board);
     end
 end
 
 # Puzzle Interfaces
 function Base.getindex(p::Puzzle, i::Int, j::Int)::Int
-    rows, cols = size(p.grid)
+    rows, cols = size(p.board)
     if (1 ≤ i ≤ rows) && (1 ≤ j ≤ cols)
-        return p.grid[i,j];
+        return p.board[i,j];
     end
     throw(BoundsError(p));
 end
 
 function Base.setindex!(p::Puzzle, n::Int, i::Int, j::Int)
-    rows, cols = size(p.grid)
+    rows, cols = size(p.board)
     if (1 ≤ i ≤ rows) && (1 ≤ j ≤ cols)
-        p.grid[i,j] = n;
+        p.board[i,j] = n;
     else
         throw(BoundsError(p));
     end
@@ -33,15 +33,18 @@ end
 function Base.show(io::IO, p::Puzzle)::Nothing
     println("\033[2J") # clear repl
     println("\033[$(displaysize(stdout)[1])A")
-    rows, cols = size(p.grid)
-    println("\t" * "-"^37)
+    rows, cols = size(p.board)
     for i in 1:rows
         for j in 1:cols
-            if j == 1
-                print("\t|")
+            print(" $(p.board[i,j]) ")
+            if (j % 3 == 0) && (j != cols)
+                print("|")
             end
-            print(" $(p.grid[i,j]) |")
         end
-        println("\n\t" * "-"^37)
+        println() # new line
+        if (i % 3 == 0) && (i != rows)
+            # draw border lines
+            println(("-"^9 * "+")^2 * "-"^9)
+        end
     end
 end
--- src/solver.jl
+++ src/solver.jl
@@ -1,9 +1,56 @@
 # Sudoku: Recursion + Backtracking
 export solve
 
-# helper function
-function possible(p::Puzzle, i::Int, j::Int, n::Int)
+# Helper Function
+function possible(p::Puzzle, x::Int, y::Int, n::Int)::Bool
+    #=
+    This function checks that the number n
+    is not already in the row x, column y
+    or the 3 x 3 sub-grid of the board.
+    =#
+    for j in 1:9
+        if p[x,j] == n
+            return false;
+        end
+    end
+    for i in 1:9
+        if p[i,y] == n
+            return false;
+        end
+    end
+    # get sub-grid coordinates
+    xs = Int.(3 * ceil(x / 3 - 1) .+ collect(1:3))
+    ys = Int.(3 * ceil(y / 3 - 1) .+ collect(1:3))
+    for i in xs
+        for j in ys
+            if p[i,j] == n
+                return false;
+            end
+        end
+    end
+    return true;
 end
 
-function solve(p::Puzzle)
+# Solve the Sudoku Puzzle
+function solve(p::Puzzle)::Bool
+    for i in 1:9
+        for j in 1:9
+            if p[i,j] == 0
+                for n in 1:9
+                    if possible(p, i, j, n)
+                        p[i,j] = n
+                        # recursion
+                        if solve(p)
+                            return true;
+                        else
+                            # backtrack
+                            p[i,j] = 0
+                        end
+                    end
+                end
+                return false;
+            end
+        end
+    end
+    return true;
 end
